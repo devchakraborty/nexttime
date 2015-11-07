10434dea45c587d988a7934c4a55cac6b7f5f1ef
devchakraborty <devchakraborty@users.noreply.github.com>
2015-11-06 23:05:07 -0500

    Initial commit

---
+++ nexttime/nexttime/SingleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  nexttime
+//
+//  Created by Dev Chakraborty on 2015-11-06.
+//  Copyright © 2015 NextTime. All rights reserved.
+//
+
+import UIKit
+
+class SingleViewController: UIViewController {
+    
+    @IBOutlet weak var segmentControl:UISegmentedControl?
+    @IBOutlet weak var textField:UITextField?
+    @IBOutlet weak var textView:UITextView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
