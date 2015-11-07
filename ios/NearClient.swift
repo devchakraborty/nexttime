ffb9ab7685bda5b3d7aa5558e31bbef5ed39deac
Arkady Arkhangorodsky <arkady.arkhangorodsky@mail.utoronto.ca>
2015-11-07 01:36:07 -0500

    Implemented Apple (lol) Maps search

--- ios/NearClient.swift
+++ ios/NearClient.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class NearClient {
     var reminders : [Reminder]
+    let distanceThreshold = 100.0 // metres
     
     init() {
         reminders = [Reminder]()
@@ -19,7 +22,54 @@ class NearClient {
         reminders.append(reminder)
     }
     
-    func checkReminders() {
+    func getClosestSearchResult(location: CLLocation, searchQuery: String)->CLLocation? {
+        let searchRequest = MKLocalSearchRequest()
         
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let location2d = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let searchRegion = MKCoordinateRegion(center: location2d, span: span)
+        
+        searchRequest.naturalLanguageQuery = searchQuery
+        searchRequest.region = searchRegion
+        
+        let search = MKLocalSearch(request: searchRequest)
+        var closestMatch : CLLocation?
+        
+        search.startWithCompletionHandler({(response: Optional<MKLocalSearchResponse>,
+            error: Optional<NSError>) in
+            
+            if error != nil {
+                print("Error occured in search: \(error!.localizedDescription)")
+            } else if response!.mapItems.count == 0 {
+                print("No matches found")
+            } else {
+                print("Matches found")
+                
+                for item in response!.mapItems {
+                    let itemLocation = item.placemark.location
+                    if (itemLocation?.distanceFromLocation(location) < self.distanceThreshold) {
+                        // Could also get the closest, rather than just the first matching
+                        closestMatch = itemLocation
+                    }
+                }
+            }
+        })
+        
+        return closestMatch
+    }
+    
+    func checkReminders(currentLocation: CLLocation)->[Reminder] {
+        var matchingReminders = [Reminder]()
+        // for each reminder in reminders,
+        //    Google Maps search for the reminder query (getting closest match)
+        //    See if within 100m, if so add to matching reminders
+        for reminder in reminders {
+            let closestSearchResult = getClosestSearchResult(currentLocation, searchQuery: reminder.specifier)
+            if closestSearchResult != nil {
+                matchingReminders.append(reminder)
+            }
+        }
+        return matchingReminders
     }
 }
+    
