9fbb316048ca27d066a6878a27c90e715d07217b
kasrakoushan <kasra.koushan@gmail.com>
2015-11-07 12:14:33 -0500

    Fix accidental merge commit

--- ios/nexttime/WithClient.swift
+++ ios/nexttime/WithClient.swift
@@ -7,8 +7,6 @@
 //
 
 import UIKit
-import CoreLocation
-// import something for Parse
 
 class WithClient {
     var reminders : [Reminder]
@@ -19,21 +17,5 @@ class WithClient {
     
     func addReminder(reminder: Reminder) {
         reminders.append(reminder)
-    }
-    
-    func findCloseFriends(location: CLLocation, onReminderTriggered: (Reminder) -> Void) {
-        // IMPLEMENT
-        // search for location data (from Parse) for friends in your reminder list
-        // if they are close, activate reminder
-        for reminder in self.reminders {
-            if friend(reminder.specifier).distance < 100 {
-               onReminderTriggered(reminder)
-            }
-        }
-    }
-    
-    func checkReminders(currentLocation: CLLocation, onReminderTriggered: (Reminder) -> Void) {
-        // find which reminders should be activated (wrapper for findCloseFriends)
-        findCloseFriends(currentLocation, onReminderTriggered: onReminderTriggered)
     }
 }
