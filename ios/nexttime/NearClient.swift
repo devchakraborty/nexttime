70f6e327e64c53ffd252a4035ed755b6256dd357
kasrakoushan <kasra.koushan@gmail.com>
2015-11-07 12:21:57 -0500

    Fb login changes

--- ios/nexttime/NearClient.swift
+++ ios/nexttime/NearClient.swift
@@ -1,5 +1,5 @@
 //
-//  NearClient.swift
+//  WithClient.swift
 //  nexttime
 //
 //  Created by Arkady Arkhangorodsky on 2015-11-06.
@@ -7,3 +7,33 @@
 //
 
 import UIKit
+import CoreLocation
+// import something for Parse
+
+class WithClient {
+    var reminders : [Reminder]
+    
+    init() {
+        reminders = [Reminder]()
+    }
+    
+    func addReminder(reminder: Reminder) {
+        reminders.append(reminder)
+    }
+    
+    func findCloseFriends(location: CLLocation, onReminderTriggered: (Reminder) -> Void) {
+        // IMPLEMENT
+        // search for location data (from Parse) for friends in your reminder list
+        // if they are close, activate reminder
+        for reminder in self.reminders {
+            if friend(reminder.specifier).distance < 100 {
+               onReminderTriggered(reminder)
+            }
+        }
+    }
+    
+    func checkReminders(currentLocation: CLLocation, onReminderTriggered: (Reminder) -> Void) {
+        // find which reminders should be activated (wrapper for findCloseFriends)
+        findCloseFriends(currentLocation, onReminderTriggered: onReminderTriggered)
+    }
+}
